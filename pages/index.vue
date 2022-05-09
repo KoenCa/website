5fe731c4619385a0f5ce57d6e264a179302fa369
Koen Castermans <castermanskoen@msn.com>
2022-05-09 22:45:52 +0200

    Figuring out some stuff

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+const SECTIONS = [
+  {
+    id: "aboutSection",
+    title: "About",
+  },
+  {
+    id: "educationSection",
+    title: "Education",
+  },
+  {
+    id: "workSection",
+    title: "Work Experience",
+  },
+  {
+    id: "developmentExperienceSection",
+    title: "Development experience",
+  },
+  {
+    id: "sideProjectsSection",
+    title: "Side projects",
+  },
+]
+
+const activeSection = ref(SECTIONS[0].id)
+const moveBodyForSidebar = ref(false)
+
+</script>
